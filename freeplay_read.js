1b72e65b2d1fe0adf89a04996f0304600c9584bf
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-08-14 17:45:14 +0800

    they updated it :( pt 2

--- freeplay_read.js
+++ freeplay_read.js
@@ -5,11 +5,11 @@ main();
 async function main() {
     read().then((res) => {
         console.log(res);
-        fs.writeFile('freeplay.txt', JSON.stringify(res), function () {});
+        fs.writeFile('freeplay.json', JSON.stringify(res), function () {});
     });
 }
 function read() {
-    return new Promise((resolve, rej) => {
+    return new Promise((resolve, vrej) => {
         res = [];
         for (const filename of files) {
             fs.readFile(`./raw/${filename}`, 'utf8', function (err, data) {
@@ -21,7 +21,7 @@ function read() {
 
                 if (bounds[0].upperBounds > 100) {
                     obj.bloon = parseBloons(filename);
-                    obj.number = FBGM.bloonEmissions_.length;
+                    obj.number = FBGM.bloonEmissions.length;
 
                     for (i = 0; i < bounds.length; i++) {
                         let bound = bounds[i];
@@ -40,10 +40,11 @@ function read() {
 function parseBloons(filename) {
     str = filename.substring(23);
     str = str.replace('.json', '');
-    str = str.replace(/\(R\d+\+*\)/, '');
-    str = str.replace('(Test)', '');
-    str = str.replace('_', '');
-    str = str.replace(/x\d+/g, '');
-    str = str.replace(' ', '');
+    str = str.replace(/\(R\d+\+*\)/, ''); // remove (R120) and related
+    str = str.replace(/\[\d+-\d+\],*/g, ''); // remove [201-262], and related
+    str = str.replace('(Test)', ''); // remove Test
+    str = str.replace('_', ''); // remove underscores
+    str = str.replace(/x\d+/g, ''); // remove x1, x2, x3, x5, x120, etc...
+    str = str.replace(/\s+/g, '');
     return str;
 }
